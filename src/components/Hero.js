95c4f7056edf0d31290abf7d8ef72f5bf51cf945
SAFIULLAH999 <safiullahgamer7543@gmail.com>
2025-06-27 23:33:13 +0500

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -23,7 +23,6 @@ const Hero = () => {
                 <span className="and-text">AND</span>
                 <span className="pill-text">LOREM IPSUM</span>
               </div>
-              <div></div>
             </h1>
             <p className="hero-description">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
